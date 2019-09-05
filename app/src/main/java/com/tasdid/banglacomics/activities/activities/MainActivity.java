1722c3827655eb8708084aa1128a6fbb33c13256
TasdidRahman <rahman.tasdid@gmail.com>
2019-09-06 00:53:30 +0600

    Bangla Comics Initial commit

---
+++ app/src/main/java/com/tasdid/banglacomics/activities/activities/MainActivity.java
@@ -0,0 +1,102 @@
+package com.tasdid.banglacomics.activities.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import com.tasdid.banglacomics.R;
+import com.tasdid.banglacomics.activities.model.Anime;
+import com.tasdid.banglacomics.activities.adapters.RecyclerViewAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String JSON_URL = "https://api.myjson.com/bins/p3i6v" ;
+    private JsonArrayRequest request ;
+    private RequestQueue requestQueue ;
+    private List<Anime> lstAnime ;
+    private RecyclerView recyclerView ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lstAnime = new ArrayList<>() ;
+        recyclerView = findViewById(R.id.recyclerviewid);
+        jsonrequest();
+
+
+
+    }
+
+    private void jsonrequest() {
+
+        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                JSONObject jsonObject  = null ;
+
+                for (int i = 0 ; i < response.length(); i++ ) {
+
+
+                    try {
+                        jsonObject = response.getJSONObject(i) ;
+                        Anime anime = new Anime() ;
+                        anime.setName(jsonObject.getString("name"));
+                        anime.setDescription(jsonObject.getString("description"));
+                        anime.setRating(jsonObject.getString("Rating"));
+                        anime.setCategorie(jsonObject.getString("categorie"));
+                        anime.setNb_episode(jsonObject.getInt("episode"));
+                        anime.setStudio(jsonObject.getString("studio"));
+                        anime.setImage_url(jsonObject.getString("img"));
+                        lstAnime.add(anime);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+
+                setuprecyclerview(lstAnime);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        requestQueue.add(request) ;
+
+
+    }
+
+    private void setuprecyclerview(List<Anime> lstAnime) {
+
+
+        RecyclerViewAdapter myadapter = new RecyclerViewAdapter(this,lstAnime) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myadapter);
+
+    }
+}
